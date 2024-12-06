e01753c29b9f5b3233b26e4788a1a209a00be3f1
Vikram <vikramjit360@gmail.com>
2024-12-06 15:47:29 +0530

    Solve GFG/Arrays/ - Strongest Neighbour

---
+++ GFG/Arrays/Strongest_Neighbour.java
@@ -0,0 +1,15 @@
+class StrongestNeighbour {
+
+    // Function to find maximum for every adjacent pairs in the array.
+    static void maximumAdjacent(int sizeOfArray, int arr[]) {
+
+        /*********************************
+         * Your code here
+         * Function should print max adjacents for all pairs
+         * Use string buffer for fast output
+         ***********************************/
+        for (int i = 0; i < sizeOfArray - 1; i++) {
+            System.out.print(Math.max(arr[i], arr[i + 1]) + " ");
+        }
+    }
+}
