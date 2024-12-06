0927f2c9688cc268fc2ee19d0498356556a72137
Vikram <vikramjit360@gmail.com>
2024-12-06 15:16:46 +0530

    Solve GFG/Arrays/ - Betting Game

---
+++ GFG/Arrays/Betting_Game.java
@@ -0,0 +1,20 @@
+class Solution {
+    static int betBalance(String result) {
+        int sum = 4;
+        int betAmt = 1;
+        for (char c : result.toCharArray()) {
+            if (sum < betAmt)
+                return -1;
+
+            if (c == 'W') {
+                sum += betAmt;
+                betAmt = 1;
+            } else if (c == 'L') {
+                sum -= betAmt;
+                betAmt *= 2;
+            }
+        }
+
+        return sum;
+    }
+}
