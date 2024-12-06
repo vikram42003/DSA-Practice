229dc04f33a2d2b9d2c3512b9e0b9fb1b7ed603b
Vikram <vikramjit360@gmail.com>
2024-12-06 22:42:44 +0530

    Solve GFG/Searching Array Search

---
+++ GFG/Searching/Array_Search.java
@@ -0,0 +1,10 @@
+class Solution {
+
+    static int search(int arr[], int x) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+}
