77103fb82eb615015eb48aacaf224ef76dc60800
Vikram <vikramjit360@gmail.com>
2024-12-06 22:58:40 +0530

    Solve GFG/Searching - Sorted Array Search

---
+++ GFG/Searching/Sorted_Array_Search.java
@@ -0,0 +1,18 @@
+class Solution {
+    static boolean searchInSorted(int arr[], int k) {
+        int l = 0, r = arr.length - 1;
+
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+            if (arr[mid] == k) {
+                return true;
+            } else if (arr[mid] > k) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return false;
+    }
+}
