b6d3144342043ee5fe6e2b97a6a7bb3154bd6d9c
Vikram <vikramjit360@gmail.com>
2024-12-07 22:34:41 +0530

    Solve GFG/Searching/ - Count 1's in binary array

---
+++ GFG/Searching/Count_1s_In_Binary_Array.java
@@ -0,0 +1,24 @@
+class Solution {
+    // Function to count number of ones in the binary array
+    // N: size of array
+    // arr[]: input array
+    public static int countOnes(int arr[], int N) {
+        int l = 0, r = N - 1;
+        int toSearch = 1, rightMost = -1;
+
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+            if (arr[mid] == toSearch) {
+                if (rightMost == -1 || mid > rightMost)
+                    rightMost = mid;
+                l = mid + 1;
+            } else if (arr[mid] > toSearch) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+
+        return rightMost + 1;
+    }
+}
