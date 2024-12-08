83e6faabe93895c94ad9234cd963682ef5d798a5
Vikram <vikramjit360@gmail.com>
2024-12-08 22:23:48 +0530

    Solve GFG/Searching/ - Roof Top

---
+++ GFG/Searching/Roof_Top.java
@@ -0,0 +1,18 @@
+class Solution {
+    // Function to find maximum number of consecutive steps
+    // to gain an increase in altitude with each step.
+    public int maxStep(int arr[]) {
+        int steps = 0, maxSteps = 0;
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] < arr[i]) {
+                steps++;
+                maxSteps = Math.max(maxSteps, steps);
+            } else {
+                steps = 0;
+            }
+        }
+
+        return maxSteps;
+    }
+}
