99766c6db6373df982d35141e15f5f7243f490e3
Vikram <vikramjit360@gmail.com>
2024-12-28 23:51:10 +0530

    Solve Leetcode/ - 852. Peak Index in a Mountain Array

---
+++ Leetcode/Peak_Index_in_a_Mountain_Array_852.java
@@ -0,0 +1,20 @@
+class Peak_Index_in_a_Mountain_Array_852 {
+    public int peakIndexInMountainArray(int[] arr) {
+        int peak = 0, l = 0, r = arr.length - 1;
+
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+            int next = mid < arr.length - 1 ? arr[mid + 1] : -1;
+
+            peak = arr[mid] > arr[peak] ? mid : peak;
+
+            if (arr[mid] > next) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return peak;
+    }
+}
