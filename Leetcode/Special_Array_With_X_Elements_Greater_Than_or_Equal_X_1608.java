6a916860c219106a90ef46a03adc3db3de79adab
Vikram <vikramjit360@gmail.com>
2025-01-02 22:35:01 +0530

    Solve Leetcode/ - 1608. Special Array With X Elements Greater Than or Equal X

---
+++ Leetcode/Special_Array_With_X_Elements_Greater_Than_or_Equal_X_1608.java
@@ -0,0 +1,14 @@
+public class Special_Array_With_X_Elements_Greater_Than_or_Equal_X_1608 {
+    public int specialArray(int[] nums) {
+        for (int i = 1; i <= nums.length; i++) {
+            int count = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (i <= nums[j])
+                    count++;
+            }
+            if (count == i)
+                return i;
+        }
+        return -1;
+    }
+}
