a3d65903cfc01019da43f03af59018a303d8d4b7
Vikram <vikramjit360@gmail.com>
2025-01-02 22:10:59 +0530

    Solve Leetcode/ - 1346. Check If N and Its Double Exist

---
+++ Leetcode/Check_If_N_and_Its_Double_Exist_1346.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class Check_If_N_and_Its_Double_Exist_1346 {
+    public boolean checkIfExist(int[] arr) {
+        Arrays.sort(arr);
+        int zeroCount = 0;
+        for (int num : arr) {
+            if (num == 0) {
+                zeroCount++;
+                if (zeroCount == 2)
+                    return true;
+                continue;
+            }
+            if (Arrays.binarySearch(arr, num * 2) >= 0)
+                return true;
+        }
+        return false;
+    }
+}
