6f35ab5d22a28622a7e01987f8bdc79e05c9d36b
Vikram <vikramjit360@gmail.com>
2025-01-05 23:32:33 +0530

    Solve Leetcode/ - 81. Search in Rotated Sorted Array II

---
+++ Leetcode/Search_in_Rotated_Sorted_Array_II_81.java
@@ -0,0 +1,31 @@
+// link - https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+
+public class Search_in_Rotated_Sorted_Array_II_81 {
+    public boolean search(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[l] == nums[r]) {
+                l++;
+            } else if (nums[l] <= nums[mid]) {
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (target <= nums[r] && target > nums[mid]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+}
