596965229e3bbe341a4cdc104c7e7f3a7c0d0473
Vikram <vikramjit360@gmail.com>
2025-01-08 19:41:55 +0530

    Solve Leetcode/ - 1283. Find the Smallest Divisor Given a Threshold

---
+++ Leetcode/Find_the_Smallest_Divisor_Given_a_Threshold_1283.java
@@ -0,0 +1,27 @@
+// link - https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
+
+public class Find_the_Smallest_Divisor_Given_a_Threshold_1283 {
+    public int smallestDivisor(int[] nums, int threshold) {
+        int l = 1, r = (int) 1e6;
+
+        while (l < r) {
+            int mid = l + ((r - l) / 2);
+            if (check(mid, nums, threshold)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+
+        return l;
+    }
+
+    public boolean check(int mid, int[] nums, int threshold) {
+        int total = 0;
+        for (int num : nums) {
+            total += num / mid;
+            total += num % mid == 0 ? 0 : 1;
+        }
+        return total <= threshold;
+    }
+}
