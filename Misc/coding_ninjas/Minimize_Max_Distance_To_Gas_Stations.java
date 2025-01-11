37f5e3dacbffd0a306ec43acc461f068c69ee1f1
Vikram <vikramjit360@gmail.com>
2025-01-12 00:15:35 +0530

    Solve Minimize Max Distance to Gas Station - Optimal Binary Search Approach

--- Misc/coding_ninjas/Minimize_Max_Distance_To_Gas_Stations.java
+++ Misc/coding_ninjas/Minimize_Max_Distance_To_Gas_Stations.java
@@ -4,6 +4,37 @@ import java.util.PriorityQueue;
 
 class Minimize_Max_Distance_To_Gas_Stations {
 
+    public static double MinimiseMaxDistance(int[] arr, int K) {
+        // Binary Search Solution - Time = O(n log n) - Space = O(1)
+        double l = 0, r = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            r = Math.max(arr[i + 1] - arr[i], r);
+        }
+
+        while (r - l > 1e-6) {
+            double mid = l + ((r - l) / 2.0);
+
+            if (check(mid, arr, K)) {
+                r = mid;
+            } else {
+                l = mid;
+            }
+        }
+
+        return r;
+    }
+
+    public static boolean check(double mid, int[] arr, int K) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            double dist = arr[i + 1] - arr[i];
+            int placed = (int) (dist / mid);
+            if (dist % mid == 0)
+                placed--;
+            K -= placed;
+        }
+        return K >= 0;
+    }
+
     public static class Pair {
         double key;
         int value;
