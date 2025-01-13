87eabe95d228d65e7c4316b271a85f72602ee415
Vikram <vikramjit360@gmail.com>
2025-01-13 11:38:57 +0530

    Solve Leetcode/ - 74. Search a 2D Matrix - linear binary search approach

--- Leetcode/_74_Search_a_2D_Matrix.java
+++ Leetcode/_74_Search_a_2D_Matrix.java
@@ -1,7 +1,35 @@
 // link - https://leetcode.com/problems/search-a-2d-matrix/description/
 
+// 1 3 5 7 10 11 16 20 23 30 34 60
+
 public class _74_Search_a_2D_Matrix {
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 60 }}; // ans = false
+        int target = 13;
+
+        System.out.println(searchMatrix(matrix, target));
+    }
+    
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int l = 0, r = matrix.length * matrix[0].length - 1;
+        while (l <= r) {
+            int mid = l + ((r - l) / 2);
+            int curr = matrix[mid / matrix[0].length][mid % matrix[0].length];
+            if (curr == target) {
+                return true;
+            } else if (curr < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        
+        return false;
+    }
+
+    public boolean searchMatrixBinarySearch(int[][] matrix, int target) {
         int rowL = 0, rowR = matrix.length - 1;
         int row = 0;
         while (rowL <= rowR) {
