dfea01e0ecf46bafcfef87043b47db797a24e8d9
Vikram <vikramjit360@gmail.com>
2025-01-21 09:18:00 +0530

    Solve Leetcode - 252. Meeting Rooms

---
+++ Leetcode/python/_252_Meeting_Rooms.py
@@ -0,0 +1,17 @@
+# Definition of Interval:
+from typing import List
+
+
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x: x.start)
+        for i in range(1, len(intervals)):
+            if intervals[i - 1].end > intervals[i].start:
+                return False
+        return True
