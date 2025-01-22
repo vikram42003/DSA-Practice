bb21e0ee05260f32843b7b8fa5953bfd78c41dd4
Vikram <vikramjit360@gmail.com>
2025-01-22 12:43:47 +0530

    Solve Leetcode/ - 8. String to Integer (atoi) - A bit Faster Approach

--- Leetcode/_8_String_to_Integer_atoi.java
+++ Leetcode/_8_String_to_Integer_atoi.java
@@ -36,4 +36,52 @@ public class _8_String_to_Integer_atoi {
             return (int) num;
         }
     }
+
+    // Faster
+    public int myAtoiFaster(String s) {
+        if (s.length() == 0)
+            return 0;
+
+        int i = 0;
+        int sign = 1;
+
+        while (i < s.length() && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i >= s.length())
+            return 0;
+
+        if (s.charAt(i) == '-') {
+            i++;
+            sign = -1;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        int sum = 0;
+        while (i < s.length() && (s.charAt(i) >= '0' && s.charAt(i) <= '9')) {
+            if (sum > Integer.MAX_VALUE / 10) {
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            sum *= 10;
+
+            if (sum > Integer.MAX_VALUE - (s.charAt(i) - '0')) {
+                if (sign == 1) {
+                    return Integer.MAX_VALUE;
+                } else {
+                    return Integer.MIN_VALUE;
+                }
+            }
+            sum += s.charAt(i) - '0';
+
+            i++;
+        }
+
+        return sum * sign;
+    }
 }
