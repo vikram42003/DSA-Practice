1499652e24260e4080f9a34297ee55e6cc5a219c
Vikram <vikramjit360@gmail.com>
2025-01-26 23:10:19 +0530

    Solve GFG/ - Count Linked List Nodes

---
+++ GFG/Count_Linked_List_Nodes.java
@@ -0,0 +1,15 @@
+// link - https://www.geeksforgeeks.org/problems/count-nodes-of-linked-list/0
+
+package GFG;
+
+public class Count_Linked_List_Nodes {
+    public int getCount(Node head) {
+        int count = 0;
+        Node curr = head;
+        while (curr != null) {
+            count++;
+            curr = curr.next;
+        }
+        return count;
+    }
+}
