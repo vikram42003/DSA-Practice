9689c7ce298a4fe22afecaa5e212fc4d8a955b47
Vikram <vikramjit360@gmail.com>
2025-01-26 21:57:32 +0530

    Solve GFG/ - Array to Linked List

---
+++ GFG/Array_to_Linked_List.java
@@ -0,0 +1,33 @@
+// link - https://www.geeksforgeeks.org/problems/introduction-to-linked-list/1
+
+package GFG;
+
+class Node {
+    int data;
+    Node next;
+
+    Node() {
+        data = 0;
+    }
+
+    Node(int d) {
+        data = d;
+    } // constructor to create a new node
+}
+
+public class Array_to_Linked_List {
+
+    static Node constructLL(int arr[]) {
+        Node head = new Node();
+
+        Node temp = head;
+        temp.data = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            temp.next = new Node(arr[i]);
+            temp = temp.next;
+        }
+
+        return head;
+    }
+}
