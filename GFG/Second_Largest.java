bbdd673820cd361f818d269e8b3c30267b17c3e6
Vikram <vikramjit360@gmail.com>
2025-01-31 00:00:43 +0530

    Solve GFG/ - Second Largest

--- GFG/Second_Largest.java
+++ GFG/Second_Largest.java
@@ -3,5 +3,23 @@
 package GFG;
 
 public class Second_Largest {
+    public int getSecondLargest(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int secMax = Integer.MIN_VALUE;
 
+        for (int i : arr) {
+            if (i > max) {
+                secMax = max;
+                max = i;
+            } else if (i > secMax && i < max) {
+                secMax = i;
+            }
+        }
+
+        if (secMax == Integer.MIN_VALUE) {
+            return -1;
+        }
+
+        return secMax;
+    }
 }
