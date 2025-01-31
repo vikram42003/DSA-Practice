35b94a22f16c85b19bc18d11ed792d35696af60e
Vikram <vikramjit360@gmail.com>
2025-01-31 22:01:30 +0530

    Solve Leetcode - 206. Reverse Linked List - Recursive approach

--- Leetcode/_206_Reverse_Linked_List.java
+++ Leetcode/_206_Reverse_Linked_List.java
@@ -22,11 +22,25 @@ class _206_Reverse_Linked_List {
         ListNode prev = null;
         ListNode curr = head;
         while (curr != null) {
-        ListNode next = curr.next;
-        curr.next = prev;
-        prev = curr;
-        curr = next;
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
+    }
+
+    public ListNode reverseListRec(ListNode head) {
+        return reverseListRecursive(null, head, null);
+    }
+
+    public ListNode reverseListRecursive(ListNode prev, ListNode curr, ListNode newHead) {
+        if (curr == null) {
+            newHead = prev;
+            return newHead;
+        }
+        newHead = reverseListRecursive(curr, curr.next, newHead);
+        curr.next = prev;
+        return newHead;
     }
 }
