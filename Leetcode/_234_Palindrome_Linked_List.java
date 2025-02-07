e3ff99558a8b34cd9a63ea4a590eafb3bd43a152
Vikram <vikramjit360@gmail.com>
2025-02-07 15:00:01 +0530

    Solve Leetcode/ - 234. Palindrome Linked List - Reverse the right half appraoch

--- Leetcode/_234_Palindrome_Linked_List.java
+++ Leetcode/_234_Palindrome_Linked_List.java
@@ -3,7 +3,8 @@
 import java.util.ArrayList;
 
 public class _234_Palindrome_Linked_List {
-    public boolean isPalindrome(ListNode head) {
+    // Store it as an ArrayList approach
+    public boolean isPalindromeNaive(ListNode head) {
         ArrayList<Integer> list = new ArrayList<>();
 
         while (head != null) {
@@ -17,5 +18,39 @@ public class _234_Palindrome_Linked_List {
         }
 
         return true;
+    }
+
+    // Reverse the right half appraoch
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode revHead = reverseList(slow.next);
+
+        while (head != null && revHead != null) {
+            if (head.val != revHead.val) {
+                return false;
+            }
+            head = head.next;
+            revHead = revHead.next;
+        }
+
+        return true;
+    }
+
+    public ListNode reverseList(ListNode start) {
+        ListNode prev = null;
+        while (start != null) {
+            ListNode next = start.next;
+            start.next = prev;
+            prev = start;
+            start = next;
+        }
+        return prev;
     }
 }
