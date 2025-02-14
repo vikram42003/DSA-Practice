fc361a117cccc515d1ac2b09c0f99785a6b112f9
Vikram <vikramjit360@gmail.com>
2025-02-14 18:09:37 +0530

    Solve Leetcode - 148. Sort List - Bottom-up Merge Sort Approach (O(1) space)

--- Leetcode/_148_Sort_List.java
+++ Leetcode/_148_Sort_List.java
@@ -26,7 +26,7 @@ public class _148_Sort_List {
     }
 
     // Merge Sort Approach - Time = O(n log n) - Space = O(log n)
-    public ListNode sortList(ListNode head) {
+    public ListNode sortListMergeSort(ListNode head) {
         // base case - if theres 0 or 1 elements in the list then return that
         if (head == null || head.next == null) {
             return head;
@@ -84,5 +84,103 @@ public class _148_Sort_List {
         }
 
         return dummy.next;
+    }
+
+    // Bottom-up Merge Sort approach - Time = O(n log n) - Space = O(1)
+    public ListNode sortList(ListNode head) {
+        // Handle edge cases
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // The head of our list will change as we sort it so dummy head will serve as an
+        // anchor for the entire list
+        // dummy.next will be the head of the final sorted list
+        ListNode dummy = new ListNode();
+
+        // Get the size of the list
+        dummy.next = head;
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+
+        // step will be the size of the sublist we consider in each iteration of the
+        // loop
+        // step <<= 1 will do bitwise rightshit and assign operation on step
+        // It will basically double the step or perform step *= 2 on each iteration
+        for (int step = 1; step < length; step <<= 1) {
+            // prev will be the dummy for each sub list in each iteration
+            // prev will also be the last node in the last list we merged
+            // the merge function will build the merged list starting from prev.next
+            // therefore reconnecting the list we disconnected in the split step
+            ListNode prev = dummy;
+
+            // curr will track the left part of the current list we are going to merge
+            // in each outer loop current will start from the head of the full list
+            ListNode curr = dummy.next;
+
+            while (curr != null) {
+                // left will be
+                ListNode left = curr;
+                // split the left part of the list, set left's tail's next to null
+                // and then return the element after tail or null
+                // here we assign that element to right
+                ListNode right = split(left, step);
+                // split the right part of the list, set right's tail's next to null
+                // and then return the element after tail or null
+                // here we assign that element to curr and it will be the start of the
+                // next partition we need to consider
+                curr = split(right, step);
+                // take the left part and right part and then merge them
+                // build the merged list starting at prev.next and then return the tail
+                // of the merged list
+                // that tail will serve as the point where we start building the next merged
+                // list
+                prev = merge(left, right, prev);
+            }
+        }
+
+        // dummy.next will be the start of the final list
+        return dummy.next;
+    }
+
+    public ListNode split(ListNode start, int step) {
+        if (start == null)
+            return null;
+
+        for (int i = 1; start.next != null && i < step; i++) {
+            start = start.next;
+        }
+        ListNode nextHead = start.next;
+        start.next = null;
+        return nextHead;
+    }
+
+    public ListNode merge(ListNode left, ListNode right, ListNode curr) {
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                curr.next = left;
+                left = left.next;
+                curr = curr.next;
+            } else {
+                curr.next = right;
+                right = right.next;
+                curr = curr.next;
+            }
+        }
+        if (left != null) {
+            curr.next = left;
+        }
+        if (right != null) {
+            curr.next = right;
+        }
+        // Move curr to be the last node of the merged list, this will be where we start
+        // connecting the next merged list
+        while (curr.next != null) {
+            curr = curr.next;
+        }
+        return curr;
     }
 }
