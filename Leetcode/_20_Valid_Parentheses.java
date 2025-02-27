c03d454fb1d378f35c08f40d5e7a592c0c6dd810
Vikram <vikramjit360@gmail.com>
2025-02-27 22:20:58 +0530

    Solve Leetcode - 20. Valid Parentheses

---
+++ Leetcode/_20_Valid_Parentheses.java
@@ -0,0 +1,23 @@
+// link - https://leetcode.com/problems/valid-parentheses/
+
+import java.util.Stack;
+
+public class _20_Valid_Parentheses {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || c != stack.pop()) {
+                return false;
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
