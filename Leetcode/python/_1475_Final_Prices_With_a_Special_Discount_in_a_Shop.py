fd0db8ff71031bb1728fd0886ad3028e4b734528
Vikram <vikramjit360@gmail.com>
2025-05-26 00:27:51 +0530

    Solve Leetcode - 1475. Final Prices With a Special Discount in a Shop - Monotonic Stack Approach

--- Leetcode/python/_1475_Final_Prices_With_a_Special_Discount_in_a_Shop.py
+++ Leetcode/python/_1475_Final_Prices_With_a_Special_Discount_in_a_Shop.py
@@ -11,3 +11,19 @@ class Solution:
             prices[i] -= 0 if j >= len(prices) else prices[j]
 
         return prices
+
+    # Monotonic Stack Version - Time = O(n + n) = O(n) - Space = O(n)
+    # We'll do n operations for array traversal, and another n operations for pushing/popping elements 
+    # (since we can only push/pop one element one time only)
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        stack = []
+
+        for i in range(len(prices) - 1, -1, -1):
+            while stack and stack[-1] > prices[i]:
+                stack.pop()
+
+            disc = stack[-1] if stack else 0
+            prices[i] -= disc
+            stack.append(prices[i] + disc)
+
+        return prices
