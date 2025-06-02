811b267abf164be2c076c34430d6ae770959e1b3
Vikram <vikramjit360@gmail.com>
2025-06-02 23:11:25 +0530

    Solve Leetcode - 219. Contains Duplicate II

---
+++ Leetcode/python/_219_Contains_Duplicate_II.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        hashmap = {}
+        for i in range(len(nums)):
+            if nums[i] in hashmap:
+                if abs(hashmap[nums[i]] - i) <= k:
+                    return True
+            hashmap[nums[i]] = i
+        return False
