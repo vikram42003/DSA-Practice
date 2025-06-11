edeb23b6c90c79f0a03bddc1dd6dc08b782cc15c
Vikram <vikramjit360@gmail.com>
2025-06-11 18:42:07 +0530

    Solve Leetcode - 215. Kth Largest Element in an Array - Sorting Approach

--- Leetcode/python/_215_Kth_Largest_Element_in_an_Array.py
+++ Leetcode/python/_215_Kth_Largest_Element_in_an_Array.py
@@ -3,9 +3,14 @@ from typing import List
 
 
 class Solution:
+    # Heap Approach - Time = O(n log n) - Space = O(n)
     def findKthLargest(self, nums: List[int], k: int) -> int:
         heap = [-num for num in nums]
         heapify(heap)
         for i in range(k - 1):
             heappop(heap)
         return -1 * heap[0]
+
+    # Sorting Approach - Time = O(n log n) - Space = O(1)
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return sorted(nums, reverse=True)[k - 1]
