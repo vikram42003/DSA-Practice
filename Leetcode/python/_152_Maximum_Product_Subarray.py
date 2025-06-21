aa10bb6cc782b12dc6ec5ce81c26c01433a7ad42
Vikram <vikramjit360@gmail.com>
2025-06-21 23:50:08 +0530

    Solve Leetcode - 152. Maximum Product Subarray

--- Leetcode/python/_152_Maximum_Product_Subarray.py
+++ Leetcode/python/_152_Maximum_Product_Subarray.py
@@ -8,36 +8,79 @@ class Solution:
     #   - If there are even number of negatives in nums, then product oall elements of nums would be the product of all its elements
     #   - (considering there are no zeros in the middle, otherwise the array would be divided)
     #
-    #   - If there are odd number of negatives then we need to exclude one of the negatives at and after/before the rightmost 
-    #     and the leftmost negative number   
+    #   - If there are odd number of negatives then we need to exclude one of the negatives at and after/before the rightmost
+    #     and the leftmost negative number
     #   - remember, we need there to be even number of negatives and we cannot remove any of the negative in the middle (cause
     #     the array needs to stay contiguos)
-    
+
     #   - If we encounter 0 in nums then reset the counting variables
     def maxProduct(self, nums: List[int]) -> int:
         max_max = nums[0]
         ignoring_leftmost_negative = 0
         ignoring_rightmost_negative = 0
         even_negatives = 1
-        
+
         for n in nums:
-            
+
             if n == 0:
-                max_max = max(max_max, ignoring_leftmost_negative, ignoring_rightmost_negative, even_negatives, 0)
+                max_max = max(
+                    max_max,
+                    ignoring_leftmost_negative,
+                    ignoring_rightmost_negative,
+                    even_negatives,
+                    0,
+                )
                 ignoring_leftmost_negative = 0
                 ignoring_rightmost_negative = 0
                 even_negatives = 1
-            
+
             if n < 0:
                 ignoring_rightmost_negative = even_negatives
-            
+
             even_negatives *= n
             ignoring_leftmost_negative *= n
 
             if ignoring_leftmost_negative == 0 and n < 0:
                 ignoring_leftmost_negative = 1
 
-        return max(max_max, ignoring_leftmost_negative, ignoring_rightmost_negative, even_negatives)
+        return max(
+            max_max,
+            ignoring_leftmost_negative,
+            ignoring_rightmost_negative,
+            even_negatives,
+        )
+
+    # Kadane Algo - Time = O(2n) - Space = O(1)
+    #
+    # You see the idea in the explanation above right
+    # Let me summarize again
+    # only positive numbers or even negatives == ╰(*°▽°*)╯
+    # Odd negatives                           == (┬┬﹏┬┬)
+    # So we gotta ignore either the leftmost or the rightmost negative and the numbers before/after that (cant ignore a negative
+    # in the middle cause, it gotta be a contiguos subarray and longer == better)
+    # SO LETS RUBN A GFUFKING LIOP FROM LEGT AND RIGHT AND KEEP TRACKIGN MAX_MAX AND TEHN RETURN IT AND WE GOOD
+    # If the prod gets reset or becomes negative it gets auto ignored
+    # And if there are odd negatives then the normal loop will (effectively) ignore rightmost negative and beyond, and the reverse
+    # loop will ignore the leftmost negative and before
+    def maxProduct(self, nums: List[int]) -> int:
+        max_max = nums[0]
+        prod = 1
+
+        for i in range(len(nums)):
+            prod *= nums[i]
+            max_max = max(max_max, prod)
+            if prod == 0:
+                prod = 1
+
+        prod = 1
+        for i in range(len(nums) - 1, -1, -1):
+            prod *= nums[i]
+            max_max = max(max_max, prod)
+            if prod == 0:
+                prod = 1
+
+        return max_max
+
 
 test = Solution()
 # ans = 6
