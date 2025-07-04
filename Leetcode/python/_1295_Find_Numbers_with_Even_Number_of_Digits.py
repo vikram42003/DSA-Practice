13b151e7d0a33b4ceba6495708d386290e614d4e
Vikram <vikramjit360@gmail.com>
2025-07-04 23:47:51 +0530

    Solve Leetcode - 1295. Find Numbers with Even Number of Digits - Optimized Version

--- Leetcode/python/_1295_Find_Numbers_with_Even_Number_of_Digits.py
+++ Leetcode/python/_1295_Find_Numbers_with_Even_Number_of_Digits.py
@@ -13,3 +13,12 @@ class Solution:
             if (digits & 1) == 0:
                 count += 1
         return count
+    
+    # Optimized Version - Time = O(n) - Space = O(1)
+    # nums[i] can only be 1 to 100000, so just check for number range that has even digits between those bounds 
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for n in nums:
+            if 9 < n < 100 or 999 < n < 10000 or 99999 < n:
+                count += 1
+        return count 
