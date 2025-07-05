8b5ea16cbbe3b1935601b8a9f3929b938fd5bcc1
Vikram <vikramjit360@gmail.com>
2025-07-05 17:08:11 +0530

    Solve Leetcode - 2089. Find Target Indices After Sorting Array - Optimized Version

--- Leetcode/python/_2089_Find_Target_Indices_After_Sorting_Array.py
+++ Leetcode/python/_2089_Find_Target_Indices_After_Sorting_Array.py
@@ -18,3 +18,20 @@ class Solution:
     def targetIndices(self, nums: List[int], target: int) -> List[int]:
         nums.sort()
         return [i for i in range(len(nums)) if nums[i] == target]
+
+    # Optimized - Time = O(n) - Space = O(n)
+    # Count the elements less than target, and count the number of duplicates of target
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        count, less = 0, 0
+        for n in nums:
+            if n < target:
+                less += 1
+            elif n == target:
+                count += 1
+
+        res = []
+        for i in range(count):
+            res.append(less)
+            less += 1
+
+        return res
