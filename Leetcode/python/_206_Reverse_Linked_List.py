20ecf38a9ba2bb8d537fa4455759208371fdb2b0
Vikram <vikramjit360@gmail.com>
2025-07-07 14:41:51 +0530

    Solve Leetcode - 206. Reverse Linked List - Iterative

---
+++ Leetcode/python/_206_Reverse_Linked_List.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # Iterative - Time = O(n) - Space = O(1)
+    # Its a linked list, just reverse the links and move head to last node
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        while head:
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
