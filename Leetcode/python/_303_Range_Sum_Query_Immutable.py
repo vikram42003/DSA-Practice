94affb5270a696e811206cbb4316c86d2f07a89b
Vikram <vikramjit360@gmail.com>
2025-07-11 23:50:02 +0530

    Solve Leetcode - 303. Range Sum Query - Immutable - Prefix Sum

--- Leetcode/python/_303_Range_Sum_Query_Immutable.py
+++ Leetcode/python/_303_Range_Sum_Query_Immutable.py
@@ -15,6 +15,21 @@ class NumArray:
         return total
 
 
+
+    # Prefix Sum - Time = O(n * 1) - Space = O(n)
+    # n - for calculating prefix sums, 1 - for each sumRange call
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+        self.pref = [0] * len(nums)
+        self.pref[0] = nums[0]
+        for i in range(1, len(nums)):
+            self.pref[i] = nums[i] + self.pref[i - 1]
+        print(self.pref)
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.pref[right] if left == 0 else self.pref[right] - self.pref[left - 1]
+
+
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
 # param_1 = obj.sumRange(left,right)
