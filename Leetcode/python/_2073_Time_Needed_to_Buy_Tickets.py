9a737abe4cf276bf55c1d647002f971313f6eb13
Vikram <vikramjit360@gmail.com>
2025-07-14 00:05:37 +0530

    Solve Leetcode - 2073. Time Needed to Buy Tickets - Optimized Version

--- Leetcode/python/_2073_Time_Needed_to_Buy_Tickets.py
+++ Leetcode/python/_2073_Time_Needed_to_Buy_Tickets.py
@@ -16,3 +16,22 @@ class Solution:
             i = (i + 1) % n
 
         return total
+
+    # Optimized Version
+    # Example - tickets = [2,3,2,1] and k = 2
+    # Think of it this way - if a person buys all the tickets they need, they're removed from the list right
+    # So, for all the people BEFORE k, as far as we're concerned, the longest time they'll spend in the line will be at max k,
+    # cause if the value is bigger than k, then by that time k would already be 0 and be out of the list and the question
+    # wouldve ended
+    # For all the people AFTER k, the longest time for them will be k - 1, cause once k is 0, the problem is solved, we dont care
+    # whether have all the tickets they need or not, so we only calculate how much time they'll be spending right before the
+    # iteration of the loop where k turns 0
+    # check this out for more - https://leetcode.com/problems/time-needed-to-buy-tickets/solutions/4995460/java-easy-solution-beats-100/
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        total = 0
+        for i in range(len(tickets)):
+            if i <= k:
+                total += min(tickets[i], tickets[k])
+            else:
+                total += min(tickets[i], tickets[k] - 1)
+        return total
