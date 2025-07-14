d8f83ebe12af7a405cb55fe4d746b1b440f1473a
Vikram <vikramjit360@gmail.com>
2025-07-14 13:33:58 +0530

    Solve Leetcode - 155. Min Stack - Two Arrays Approach

--- Leetcode/python/_155_Min_Stack.py
+++ Leetcode/python/_155_Min_Stack.py
@@ -21,6 +21,29 @@ class MinStack:
         return self.stack[-1][1]
 
 
+class MinStack:
+    # Two Arrays - Time = O(1) - Space = O(n)
+    def __init__(self):
+        self.stack = []
+        self.minStack = []
+
+    def push(self, val: int) -> None:
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        if self.stack[-1] == self.minStack[-1]:
+            self.minStack.pop()
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
 # Your MinStack object will be instantiated and called as such:
 # obj = MinStack()
 # obj.push(val)
