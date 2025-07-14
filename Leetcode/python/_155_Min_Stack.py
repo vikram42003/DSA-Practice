ae87cedd86b9aabfa6356a72e37bee1fad898351
Vikram <vikramjit360@gmail.com>
2025-07-14 13:33:02 +0530

    Solve Leetcode - 155. Min Stack

---
+++ Leetcode/python/_155_Min_Stack.py
@@ -0,0 +1,29 @@
+class MinStack:
+    # Single array with tuples - Time = O(1) - Space = O(n)
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.getMin())))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
