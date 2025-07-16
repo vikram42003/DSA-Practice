ab6bedde7e724ae3f9af641f225e4de6a7055c87
Vikram <vikramjit360@gmail.com>
2025-07-16 23:24:55 +0530

    Solve Leetcode - 75. Sort Colors - Bubble Sort

---
+++ Leetcode/python/_75_Sort_Colors.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    # Bubble Sort - Time = O(n^2) - Space = O(1)
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            swapped = False
+            for j in range(len(nums) - i - 1):
+                if nums[j] > nums[j + 1]:
+                    nums[j], nums[j + 1] = nums[j + 1], nums[j]
+                    swapped = True
+            if not swapped:
+                break
