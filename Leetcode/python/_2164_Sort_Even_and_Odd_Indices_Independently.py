b82a8c587ffbebe54402cf355a605aea58cb31df
Vikram <vikramjit360@gmail.com>
2025-07-16 23:05:13 +0530

    Solve Leetcode - 2164. Sort Even and Odd Indices Independently - Bubble Sort

---
+++ Leetcode/python/_2164_Sort_Even_and_Odd_Indices_Independently.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    # Bubble Sort - Time = O(n^2) - Space = O(1)
+    def sortEvenOdd(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        for j in range(0, n, 2):
+            swapped = False
+
+            for i in range(n - 2 - j):
+                if i & 1 == 0:
+                    if nums[i] > nums[i + 2]:
+                        nums[i], nums[i + 2] = nums[i + 2], nums[i]
+                        swapped = True
+                else:
+                    if nums[i] < nums[i + 2]:
+                        nums[i], nums[i + 2] = nums[i + 2], nums[i]
+                        swapped = True
+
+            if not swapped:
+                break
+
+        return nums
