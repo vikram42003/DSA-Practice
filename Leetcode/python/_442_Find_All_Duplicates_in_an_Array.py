27a3040e271ffaab1ae689dc01a0558e78805d19
Vikram <vikramjit360@gmail.com>
2025-07-17 23:05:47 +0530

    Solve Leetcode - 442. Find All Duplicates in an Array - Like Cyclic Sort but flip signs instead of sorting Approach

--- Leetcode/python/_442_Find_All_Duplicates_in_an_Array.py
+++ Leetcode/python/_442_Find_All_Duplicates_in_an_Array.py
@@ -14,9 +14,22 @@ class Solution:
                 nums[i], nums[correct] = nums[correct], nums[i]
             else:
                 i += 1
-            
+
         for i in range(len(nums)):
             if nums[i] != i + 1:
                 duplicates.append(nums[i])
+
+        return duplicates
+
+    # Like Cyclic Sort but flip signs instead of sorting - Time = O(n) - Space = O(1)
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        duplicates = []
+
+        for n in nums:
+            i = abs(n) - 1
+            if nums[i] < 0:
+                duplicates.append(abs(n))
+            else:
+                nums[i] *= -1
 
         return duplicates
