66aff00865ccf35d44371ccc18b9b5643e2f06a2
Vikram <vikramjit360@gmail.com>
2025-07-18 23:23:57 +0530

    Solve Leetcode - 1464. Maximum Product of Two Elements in an Array - Mantain 2 Max Solution

--- Leetcode/python/_1464_Maximum_Product_of_Two_Elements_in_an_Array.py
+++ Leetcode/python/_1464_Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -9,8 +9,19 @@ class Solution:
             for j in range(i + 1, len(nums)):
                 max_max = max(max_max, (nums[i] - 1) * (nums[j] - 1))
         return max_max
-    
+
     # Sort (Built in Tim Sort) - Time = O(n log n) - Space = O(n)
     def maxProduct(self, nums: List[int]) -> int:
         nums.sort()
         return (nums[-1] - 1) * (nums[-2] - 1)
+
+    # 2 Max - Time = O(n) - Space = O(n)
+    def maxProduct(self, nums: List[int]) -> int:
+        max1, max2 = 0, 0
+        for n in nums:
+            if n >= max1:
+                max2 = max1
+                max1 = n
+            elif n > max2:
+                max2 = n
+        return (max1 - 1) * (max2 - 1)
