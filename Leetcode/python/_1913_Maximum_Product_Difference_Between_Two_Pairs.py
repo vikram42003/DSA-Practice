0e2a18feaea8fae161fc0882d484462a0f7b97c2
Vikram <vikramjit360@gmail.com>
2025-07-19 20:05:41 +0530

    Solve Leetcode - 1913. Maximum Product Difference Between Two Pairs - 2 Min and 2 Max Solution

--- Leetcode/python/_1913_Maximum_Product_Difference_Between_Two_Pairs.py
+++ Leetcode/python/_1913_Maximum_Product_Difference_Between_Two_Pairs.py
@@ -5,3 +5,19 @@ class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
         nums.sort()
         return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
+
+    def maxProductDifference(self, nums: List[int]) -> int:
+        max1, max2, min1, min2 = 0, 0, 10001, 10001
+        for n in nums:
+            if n >= max1:
+                max2 = max1
+                max1 = n
+            elif n > max2:
+                max2 = n
+
+            if n <= min1:
+                min2 = min1
+                min1 = n
+            elif n < min2:
+                min2 = n
+        return (max1 * max2) - (min1 * min2)
