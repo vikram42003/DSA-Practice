ace9ff3522699ad98896fe543c7ce5af45fa8303
Vikram <vikramjit360@gmail.com>
2025-07-19 23:40:01 +0530

    Solve Leetcode - 56. Merge Intervals - Sorting (Optimized)

--- Leetcode/python/_56_Merge_Intervals.py
+++ Leetcode/python/_56_Merge_Intervals.py
@@ -2,6 +2,29 @@ from typing import List
 
 
 class Solution:
+    # Sorting (Optimized) - Time = O(n log n) - Space = O(n)
+    # same as below, except just use prev and cur instead of cur and next
+    # update the end values at each step and if cur does not overlap then add prev to res and set prev = cur
+    # at the end add prev to res cause it may still hold a value
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort()
+        res = []
+
+        prev = None
+        for cur in intervals:
+            if not prev:
+                prev = cur
+                continue
+
+            if prev[1] >= cur[0]:
+                prev[1] = max(prev[1], cur[1])
+            else:
+                res.append(prev)
+                prev = cur
+
+        res.append(prev)
+        return res
+    
     # Sorting - Time = O(n log n) - Space = O(n)
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
@@ -38,3 +61,4 @@ class Solution:
 # repeat until len(nums) - 1
 # if next.start > cur.end then the previous range is [start, cur.end] and then set start to None
 # and move to next element
+# EDGECASE - cur.end may be greater than next.end so keep updating that too
