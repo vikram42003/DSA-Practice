1aea3730e00be6557366bb4f8268049ea8c291e3
Vikram <vikramjit360@gmail.com>
2025-07-23 12:33:15 +0530

    Solve Leetcode - 23. Merge k Sorted Lists - Heap Approach

--- Leetcode/python/_23_Merge_k_Sorted_Lists.py
+++ Leetcode/python/_23_Merge_k_Sorted_Lists.py
@@ -1,3 +1,4 @@
+import heapq
 from typing import List, Optional
 
 
@@ -29,4 +30,27 @@ class Solution:
             cur.next = n
             cur = cur.next
 
+        return dummy.next
+    
+    # Heap - Time = O(n log k) - Space = O(k)
+    # The lists are already in ascending order, so only keep track of each head of the list, add the min from the current heads
+    # and then move that head forward and add its value to the pool again. This way we only store k elements and we check k elements
+    # with the heap n times
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not any(lists):
+            return None
+
+        heap = []
+        for i, l in enumerate(lists):
+            if l:
+                heapq.heappush(heap, (l.val, i, l))
+
+        dummy = cur = ListNode()
+        while heap:
+            val, i, l = heapq.heappop(heap)
+            cur.next = l
+            cur = cur.next
+            if l.next:
+                heapq.heappush(heap, (l.next.val, i, l.next))
+        
         return dummy.next
