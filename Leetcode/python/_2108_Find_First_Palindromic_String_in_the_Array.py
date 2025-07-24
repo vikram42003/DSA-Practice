1a49ff4ff6362ec07de60024b0d79e1c2c33fac6
Vikram <vikramjit360@gmail.com>
2025-07-25 01:04:50 +0530

    Solve Leetcode - 2108. Find First Palindromic String in the Array - Two Pointers

---
+++ Leetcode/python/_2108_Find_First_Palindromic_String_in_the_Array.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            i, j = 0, len(word) - 1
+            while i < j and word[i] == word[j]:
+                i += 1
+                j -= 1
+
+            if i >= j:
+                return word
+
+        return ""
