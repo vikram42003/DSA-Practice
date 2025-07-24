1dee0078351c23a5853f686f6f5605ace8f3ee91
Vikram <vikramjit360@gmail.com>
2025-07-25 00:52:16 +0530

    Solve Leetcode - 977. Squares of a Sorted Array - Two Pointers

--- Leetcode/python/_977_Squares_of_a_Sorted_Array.py
+++ Leetcode/python/_977_Squares_of_a_Sorted_Array.py
@@ -8,3 +8,16 @@ class Solution:
             nums[i] = nums[i] ** 2
         nums.sort()
         return nums
+
+    # Two Pointers - Time = O(n) - Space = O(n)
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        i, j = 0, len(nums) - 1
+        res = []
+        while i <= j:
+            if abs(nums[i]) > abs(nums[j]):
+                res.append(nums[i] ** 2)
+                i += 1
+            else:
+                res.append(nums[j] ** 2)
+                j -= 1
+        return res[::-1]
