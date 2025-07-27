dc67b9525e9462b821f2b4e6ed3a106974f3586e
Vikram <vikramjit360@gmail.com>
2025-07-27 12:17:23 +0530

    Solve Leetcode - 16. 3Sum Closest - Two Pointers

--- Leetcode/python/_16_3Sum_Closest.py
+++ Leetcode/python/_16_3Sum_Closest.py
@@ -18,3 +18,27 @@ class Solution:
                         break
 
         return ans
+
+    # Sort, N loop and Two Pointers - Time = O(n^2 log n) - Space = O(1)
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        closest = float("inf")
+        ans = 0
+
+        for i in range(len(nums)):
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                sum_ = nums[i] + nums[l] + nums[r]
+
+                if abs(target - sum_) < closest:
+                    closest = abs(target - sum_)
+                    ans = sum_
+
+                if sum_ > target:
+                    r -= 1
+                elif sum_ < target:
+                    l += 1
+                else:
+                    break
+
+        return ans
