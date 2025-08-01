19e6170d623e921460618c9f92640dbfcdd2b393
Vikram <vikramjit360@gmail.com>
2025-08-01 23:58:19 +0530

    Solve Leetcode - 226. Invert Binary Tree - BFS

---
+++ Leetcode/python/_226_Invert_Binary_Tree.py
@@ -0,0 +1,26 @@
+from collections import deque
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # BFS - Time = O(n) - Space = O(n)
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return
+        dq = deque([root])
+        while dq:
+            curr = dq.popleft()
+            if curr.left:
+                dq.append(curr.left)
+            if curr.right:
+                dq.append(curr.right)
+            curr.left, curr.right = curr.right, curr.left
+        return root
