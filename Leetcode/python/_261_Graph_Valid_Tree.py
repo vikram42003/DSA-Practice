3a12a314bb92daf4b1c19ae048ede55c5925c3d9
Vikram <vikramjit360@gmail.com>
2025-11-17 11:50:14 +0530

    Solve Leetcode - 261. Graph Valid Tree - BFS (Optimal)

--- Leetcode/python/_261_Graph_Valid_Tree.py
+++ Leetcode/python/_261_Graph_Valid_Tree.py
@@ -3,6 +3,42 @@ from typing import List
 
 
 class Solution:
+    # BFS (Optimal) - Time = O(V + E) - Space = O(V + E)
+    # A valid tree must have exactly n - 1 edges and all nodes must be connected.
+    # Build a bi-directional adjacency list and use a queue of (node, parent) and a seen set starting from 0 for traversal
+    # Edge case: return False if len(edges) != n - 1 because in a valid tree, total edges are always 1 less than the total vertices
+    # For each neighbor:
+    #   - If it's already seen and not the parent, it means its a cycle so return False.
+    #   - Else mark it seen and push it to the queue as (child, curr).
+    # Finally, return len(seen) == n to check if we visited all nodes.
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        adj = {i: [] for i in range(n)}
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        seen = set([0])
+        q = deque([(0, -1)])
+
+        while q:
+            curr, prev = q.popleft()
+
+            for child in adj[curr]:
+                if child == prev:
+                    continue
+
+                if child in seen:
+                    return False
+
+                seen.add(child)
+                q.append((child, curr))
+
+        return len(seen) == n
+
     # BFS - Time = O(V + E) - Space = O(V + E)
     # Make a bi-directional adjacency matrix (WITH SETS) and add each edge and its inverse ((0, 1) and (1, 0))
     # We'll do BFS so make a deque and a seen set for traversal and add 0 to both, then traverse LEVEL BY LEVEL and
