d9b1a39f309ea0042f2a185adfcee2f22a61ae74
Vikram <vikramjit360@gmail.com>
2025-11-30 23:47:09 +0530

    Solve Leetcode - 778. Swim in Rising Water - Dijkstra Optimal

--- Leetcode/python/_778_Swim_in_Rising_Water.py
+++ Leetcode/python/_778_Swim_in_Rising_Water.py
@@ -4,6 +4,32 @@ from typing import List
 
 class Solution:
     # Dijkstra - Time = O(n^2 log n) - Space = O(n^2)
+    # Start Dijkstra from top left, traverse in all directions, selecting shortest path and end when we hit bottm right.
+    # For distance, we'll just track the time/weight we need to traverse that particular neighbour by doing max(w, grid[nr][nc]) because
+    # we dont need to do culminative distance since the problem statement states that we can travel infinite distance as long as grid[i][j] <= t
+    # So thats why we only need to return the max value of grid[i][j] we see in the shortest path from top left to bottom right
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        heap = [(grid[0][0], 0, 0)]
+        seen = set()
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+        while heap:
+            w, r, c = heappop(heap)
+            if (r, c) in seen:
+                continue
+
+            if r == n - 1 and c == n - 1:
+                return w
+
+            seen.add((r, c))
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+                if (nr, nc) not in seen and 0 <= nr < n and 0 <= nc < n:
+                    heappush(heap, (max(w, grid[nr][nc]), nr, nc))
+
+        return -1
+
     def swimInWater(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
 
