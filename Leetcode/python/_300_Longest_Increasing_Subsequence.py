088f1ca029dd470934627b992e3881509b985fa0
Vikram <vikramjit360@gmail.com>
2026-01-01 01:11:51 +0530

    Solve Leetcode - 300. Longest Increasing Subsequence - DP + Binary Search (Patience Sorting)

--- Leetcode/python/_300_Longest_Increasing_Subsequence.py
+++ Leetcode/python/_300_Longest_Increasing_Subsequence.py
@@ -1,7 +1,35 @@
+from bisect import bisect_left
 from typing import List
 
 
 class Solution:
+    # DP + Binary Search (Patience Sorting) - Time = O(n log n) - Space = O(n)
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # State will be each possible length of the subsequence, which will have length len(nums)
+        # It will store whats the minimum possible num we can end on for each possible substring value
+        # Like for [10,9,2,5,3,7,101,18], tails[1] will be 2 because that forms the maximum increasing
+        # substring of length 1 with the minimum ending number
+        tails = []
+
+        # (Check out Patience Sorting)
+        # After that for each num, find the first position where num >= tails[i], or thinking 
+        # inversely, first value where num was bigger than some tail position, that will be the 
+        # position where we should repalce num with, if its within the array then replace, otherwise
+        # append at the end of the array
+        for num in nums:
+            idx = bisect_left(tails, num)
+
+            if idx == len(tails):
+                # num is larger than all existing tails, so it extends the longest increasing subsequence
+                tails.append(num)
+            else:
+                # num improves (lowers) the ending value of a subsequence of length idx + 1
+                # no need for any min checks, this position will be >= num
+                tails[idx] = num
+        
+        # The length of tails represents the length of the longest increasing subsequence found
+        return len(tails)
+    
     # DP (Bottom up tabulation) - Time = O(n ^ 2) - Space = O(n)
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
