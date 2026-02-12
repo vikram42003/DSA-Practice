b9cde4af2a87d754c85f9311f19db1a2efe1c7ed
Vikram <vikramjit360@gmail.com>
2026-02-12 11:58:54 +0530

    Solve Leetcode - 347. Top K Frequent Elements - Optimal

--- Leetcode/python/_347_Top_K_Frequent_Elements.py
+++ Leetcode/python/_347_Top_K_Frequent_Elements.py
@@ -2,6 +2,25 @@ from typing import Counter, List
 
 
 class Solution:
+    # Buckets and Shii - Time = O(n) - Space = O(n)
+    # To eliminate the sorting step and get the time complexity under O(n log n), we make buckets of arrays for all 
+    # the possible frequencies of numbers (max would be freq equal to len(nums)), and then put each number by frequency
+    # into its bucket, and now to get the top K, iterate over the buckets from the back, put it in res
+    # return when len(res) == k
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = Counter(nums)
+        buckets = [[] for _ in range(len(nums) + 1)]
+
+        for num, count in counter.items():
+            buckets[count].append(num)
+        
+        res = []
+        for i in range(len(buckets) - 1, -1, -1):
+            for num in buckets[i]:
+                res.append(num)
+                if len(res) == k:
+                    return res
+                
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
         return sorted(count, key=lambda x: count[x], reverse=True)[:k]
