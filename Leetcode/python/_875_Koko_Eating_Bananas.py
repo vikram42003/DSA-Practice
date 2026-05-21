1c9a48a458602ce35172c550425927181fc41a06
Vikram <vikramjit360@gmail.com>
2026-05-21 23:16:01 +0530

    Solve Leetcode - 875. Koko Eating Bananas - New Binary Search Template

--- Leetcode/python/_875_Koko_Eating_Bananas.py
+++ Leetcode/python/_875_Koko_Eating_Bananas.py
@@ -3,6 +3,24 @@ from typing import List
 
 
 class Solution:
+    # https://www.notion.so/Data-Structures-and-Algorithms-1b89e6c4959d80c28d41c20ba3a209a3?source=copy_link#3679e6c4959d80f8b987f9ca7c26199a
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        def condition(k: int) -> bool:
+            total = 0
+            for p in piles:
+                total += math.ceil(p / k)
+            return total <= h
+    
+        l, r = 1, max(piles)
+        while l < r:
+            mid = l + (r - l) // 2
+            if condition(mid):
+                r = mid
+            else:
+                l = mid + 1
+        
+        return l
+    
     # Binary Search + Check - Time = O(n log n) - Space = O(1)
     # The core idea is that we try different values for k until we find the min val of k
     # that still keeps us within h limit, and then return that val
