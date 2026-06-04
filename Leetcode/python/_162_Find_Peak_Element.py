6d2e4668bf89952c4725ba47d5f3f03b8e3de5eb
Vikram <vikramjit360@gmail.com>
2026-06-04 12:54:34 +0530

    (R) Solve Leetcode - 162. Find Peak Element

--- Leetcode/python/_162_Find_Peak_Element.py
+++ Leetcode/python/_162_Find_Peak_Element.py
@@ -13,6 +13,28 @@ class Solution:
 
         return -1
     
+    # Binary Search - Time = O(log n) - Space = O(1)
+    # The core idea for choosing the binary search is, that at any index, cur could either be the peak element
+    # or it could be lesser than prev or next element, in those cases, we move towards the larger element, and
+    # discard the other side (constraints say cur != next or cur != prev for any index)
+    # With this, we could either keep climbing until we hit the peak element, or the greater element hits 
+    # the bounds, in that case bounds mean smaller that the element so it will be the peak
+    def findPeakElement(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + (r - l) // 2
+            cur = nums[m]
+            prev = nums[m - 1] if m - 1 >= 0 else float("-inf")
+            nxt = nums[m + 1] if m + 1 <= len(nums) - 1 else float("-inf")
+
+            if cur > prev and cur > nxt:
+                return m
+            elif prev > cur:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
+    
     # Binary Search Solution - Time = O(log n) - Space = O(1)
     def findPeakElementBinarySearch(self, nums: List[int]) -> int:
         l, r = 0, len(nums) - 1
