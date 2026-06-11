ff268e3b638b1bd84767e34eb6d1efc9baa8a21c
Vikram <vikramjit360@gmail.com>
2026-06-12 01:23:13 +0530

    (R) Solve Leetcode - 19. Remove Nth Node From End of List

--- Leetcode/python/_19_Remove_Nth_Node_From_End_of_List.py
+++ Leetcode/python/_19_Remove_Nth_Node_From_End_of_List.py
@@ -7,6 +7,25 @@ class ListNode:
 
 
 class Solution:
+    # Fast and Slow - Time = O(n) - Space = O(1)
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        slow, fast = head, head
+
+        # Move fast n elements ahead of slow, so that when fast.next = null, this means that slow.next is the one we need to skip
+        for i in range(n):
+            fast = fast.next
+        
+        # If n == size of linked list, meaning the head is the element we need to remove then fast will be None, so just return head.next in that case
+        if fast:
+            while fast.next:
+                fast = fast.next
+                slow = slow.next
+            
+            slow.next = slow.next.next
+            return head
+        else:
+            return head.next
+        
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         fast = head
         while fast.next != None and n > 0:
